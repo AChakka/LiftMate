e2d62f350dc8c331fca4ee9774dcc159ccc229a8
KentoHop <kentohopkins77@gmail.com>
2025-03-22 22:30:39 -0400

    Update Classification.py

--- Classification.py
+++ Classification.py
@@ -3,4 +3,4 @@ from ultralytics import YOLO
 # Load a COCO-pretrained YOLO12n model
 model = YOLO("yolov8n-cls.pt")
 
-result = model.train(data='dataset/Excersise', epochs=20, imgsz=20)
+result = model.train(data='/~/LIFTMATE/dataset/Excersise', epochs=20, imgsz=20)
